ce8f0b03bfb170468f3a7a8df95a55c9b37492f2
Matti Tahvonen <matti@vaadin.com>
2021-02-16 12:21:39 +0200

    copy-pasted main-layout & styling form svc project

--- starter/src/main/java/com/example/starter/base/impl/MainLayout.java
+++ starter/src/main/java/com/example/starter/base/impl/MainLayout.java
@@ -1,33 +1,97 @@
 package com.example.starter.base.impl;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Location;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.RouteConfiguration;
-import com.vaadin.flow.router.RouteData;
-import com.vaadin.flow.router.RouterLayout;
-import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.tabs.TabsVariant;
+import com.vaadin.flow.router.*;
 import com.vaadin.flow.server.RouteRegistry;
 import com.vaadin.flow.shared.Registration;
 
-public class MainLayout extends HorizontalLayout implements RouterLayout {
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-    private final VerticalLayout sideBar;
+@CssImport("./styles/views/main/main-layout.css")
+public class MainLayout extends AppLayout {
 
     private Registration routesListener;
+    private final Tabs menu;
+    private H1 viewTitle;
 
     public MainLayout() {
-        sideBar = new VerticalLayout();
-        sideBar.setWidth(null);
-        add(sideBar);
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        menu = createMenu();
+        addToDrawer(createDrawerContent(menu));
+    }
+
+    private Component createHeaderContent() {
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setId("header");
+        layout.getThemeList().set("dark", true);
+        layout.setWidthFull();
+        layout.setSpacing(false);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.add(new DrawerToggle());
+        viewTitle = new H1();
+        layout.add(viewTitle);
+        layout.add(new Avatar());
+        return layout;
+    }
+
+    private Component createDrawerContent(Tabs menu) {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        layout.getThemeList().set("spacing-s", true);
+        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        HorizontalLayout logoLayout = new HorizontalLayout();
+        logoLayout.setId("logo");
+        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        logoLayout.add(new Image("images/logo.png", "My Project logo"));
+        logoLayout.add(new H1("My Project"));
+        layout.add(logoLayout, menu);
+        return layout;
+    }
+
+    private Tabs createMenu() {
+        final Tabs tabs = new Tabs();
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
+        tabs.setId("tabs");
+        return tabs;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private Optional<Tab> getTabForComponent(Component component) {
+        return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
+                .findFirst().map(Tab.class::cast);
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
     }
 
     @Override
@@ -72,7 +136,7 @@ public class MainLayout extends HorizontalLayout implements RouterLayout {
     }
 
     private void fillSideBar(UI ui) {
-        sideBar.removeAll();
+        menu.removeAll();
         RouteRegistry registry = ui.getInternals().getRouter().getRegistry();
 
         List<RouteData> routes = registry.getRegisteredRoutes().stream()
@@ -85,10 +149,11 @@ public class MainLayout extends HorizontalLayout implements RouterLayout {
     }
 
     private void addNavigationItem(RouteData data) {
-        Class<? extends Component> navigationTarget = data
-                .getNavigationTarget();
-        RouterLink item = new RouterLink(getCaption(data), navigationTarget);
-        sideBar.add(item);
+        RouterLink item = new RouterLink(getCaption(data),
+                data.getNavigationTarget());
+        final Tab tab = new Tab(item);
+        ComponentUtil.setData(tab, Class.class, data.getNavigationTarget());
+        menu.add(tab);
     }
 
     private String getCaption(RouteData data) {
